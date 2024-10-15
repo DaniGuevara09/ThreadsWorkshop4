7273bd11017fe4e41dd8fe2faeca4b64a99bed2c
Laura Daniela Guevara <laura.guevara02@uptc.edu.co>
2024-10-14 22:50:04 -0500

    principal view style

--- src/main/java/co/edu/uptc/project1priorityqueues/view/Main.java
+++ src/main/java/co/edu/uptc/project1priorityqueues/view/Main.java
@@ -2,18 +2,26 @@ package co.edu.uptc.project1priorityqueues.view;
 
 import co.edu.uptc.project1priorityqueues.model.Patient;
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
+
+import java.io.File;
 
 public class Main extends Application {
 
     private Stage primaryStage;
     private Scene scene;
-    private VBox root;
+    private BorderPane root;
 
     private Label titleLabel;
     private TableView<Patient> table;
@@ -24,19 +32,48 @@ public class Main extends Application {
 
     public Main() {
         primaryStage = new Stage();
-        root = new VBox();
+        root = new BorderPane();
+        screenWidth = Screen.getPrimary().getVisualBounds().getWidth();
+        screenHeight = Screen.getPrimary().getVisualBounds().getHeight();
+
         scene = new Scene(root, screenWidth, screenHeight);
-        titleLabel = new Label("Medicamentos don Copito");
-        addTurnBtn = new Button("Add Patient");
+        titleLabel = new Label("Don Copito Medicines");
+        addTurnBtn = new Button("Add Turn");
         table = new TableView<>();
     }
 
     @Override
     public void start(Stage stage) throws Exception {
+        scene.getStylesheets()
+                .add(new File("src/main/java/co/edu/uptc/project1priorityqueues/view/Style.css").toURI().toString());
 
+        root.setId("root");
+        root.setTop(titleLabel);
+        root.setCenter(table);
+        root.setBottom(addTurnBtn);
+
+        BorderPane.setAlignment(titleLabel, Pos.CENTER);
+        BorderPane.setAlignment(table, Pos.CENTER);
+        BorderPane.setAlignment(addTurnBtn, Pos.CENTER_RIGHT);
+
+        // Up, right, down, left
+        BorderPane.setMargin(titleLabel, new Insets(60, 0, 30, 0));
+        BorderPane.setMargin(table, new Insets(30, 60, 30, 60));
+        BorderPane.setMargin(addTurnBtn, new Insets(30, 60, 30, 0));
+
+        primaryStage.setTitle("Motorcycle Information Management");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public void table(){
+        TableColumn<Patient, String> id = new TableColumn<>("Identification number");
+        id.setCellValueFactory(new PropertyValueFactory<>("id"));
+
+        TableColumn<Patient, String> turn = new TableColumn<>("Identification number");
     }
 
     public static void main(String[] args) {
-
+        launch(args);
     }
 }
